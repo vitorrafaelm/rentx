69e6a450f5d2b37b74ff0eafb544575793eb17e6
vitorrafaelm <vitor.rafael1518@gmail.com>
2022-08-15 15:09:27 -0300

    fix: initiated splash screen config

--- src/screens/Splash/index.tsx
+++ src/screens/Splash/index.tsx
@@ -1,13 +1,65 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { StyleSheet, Dimensions } from 'react-native';
+import Animated, { useSharedValue, useAnimatedStyle, withTiming, interpolate, Extrapolate } from "react-native-reanimated";
+
+import BrandSvg from '../../assets/brand.svg';
+import LogoSvg from '../../assets/logo.svg';
+
 import {
   Container,
 } from "./styles";
 
+// Cubic bezier - Site para animações -> Usar junto com o Easing
 
 export function Splash(): JSX.Element {
+  const splashAnimation = useSharedValue(0); 
+
+  const brandStyle = useAnimatedStyle(() => {
+    return {
+      opacity: interpolate(
+        splashAnimation.value,
+        [0, 50],
+        [1, 0],
+      ),
+      transform: [
+        {
+          translateX: interpolate(splashAnimation.value, [0, 50], [0, -50], Extrapolate.CLAMP)
+        }
+      ],
+    }
+  }); 
+
+  const logoStyle = useAnimatedStyle(() => {
+    return {
+      opacity: interpolate(
+        splashAnimation.value,
+        [0, 25, 50],
+        [0, .3, 1],
+      ),
+      transform: [
+        {
+          translateX: interpolate(splashAnimation.value, [0, 50], [-50, 0], Extrapolate.CLAMP)
+        }
+      ],
+    }
+  });
+
+  useEffect(() => {
+    splashAnimation.value = withTiming(
+      50, 
+      { duration: 1000 }
+    );
+  }, []); 
+
   return (
     <Container>
-      
+      <Animated.View style={[brandStyle, { position: 'absolute' }]}>
+        <BrandSvg width={80} height={50} />
+      </Animated.View>
+
+      <Animated.View style={[logoStyle,  { position: 'absolute' }]}>
+        <LogoSvg width={180} height={20} />
+      </Animated.View>
     </Container>
   );
 }
